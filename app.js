81c8820809686b049af7171f02616309e6b3308c
Sam <81008049+sam9750@users.noreply.github.com>
2022-01-03 06:20:47 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,34 @@
+function aFunction() {
+    const element = document.body;
+    element.classList.toggle("red");
+ }
+
+ function bFunction() {
+    const element = document.body;
+    element.classList.toggle("orange");
+ }
+
+ function cFunction() {
+    const element = document.body;
+    element.classList.toggle("yellow");
+ }
+
+ function dFunction() {
+    const element = document.body;
+    element.classList.toggle("green");
+ }
+
+ function eFunction() {
+    const element = document.body;
+    element.classList.toggle("blue");
+ }
+
+ function fFunction() {
+    const element = document.body;
+    element.classList.toggle("indigo");
+ }
+ 
+ function gFunction() {
+    let element = document.body;
+    element.classList.toggle("violet");
+ }
